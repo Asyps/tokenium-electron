cba5f16cbb4ae2713c88e5ffa38e6a7ed44a371b
Asyps <105502574+Asyps@users.noreply.github.com>
2024-07-09 14:19:18 +0200

    test 2

--- main.js
+++ main.js
@@ -1,31 +1,11 @@
-const { app, BrowserWindow, BrowserView } = require('electron')
+const { app, BrowserWindow } = require('electron')
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 800, height: 600
     })
 
-    const view1 = new BrowserView()
-    const view2 = new BrowserView()
-
-    view1.setBounds({
-        x: 0,
-        y: 0,
-        width: 400,
-        height: 600
-    })
-    view2.setBounds({
-        x: 400,
-        y: 0,
-        width: 400,
-        height: 600
-    })
-
-    view1.webContents.loadFile("index.html")
-    view2.webContents.loadFile("susdex.html")
-
-    win.addBrowserView(view1)
-    win.addBrowserView(view2)
+    win.loadFile("index.html")
 }
 
 app.whenReady().then(() => {
